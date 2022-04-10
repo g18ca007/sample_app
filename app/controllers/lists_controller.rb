1e1f7f9ebdba5f84b0dd3833d9d4ee058fe089b3
g18ca007 <g18ca007@gmail.com>
2022-04-10 00:56:53 +0000

    [Finish] 3-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,17 @@
 class ListsController < ApplicationController
   def new
+    # Viewへ渡すためのインスタン変数に空のModelオブジェクトを生成する
+    @list = List.new
   end
+
+def create
+  # 1.&2. データを受け取り新規登録するためのインスタンス作成
+  list = List.new(list_params)
+  # 3.データをデータベースに保存するためのsaveメゾット実行
+  list.save
+  # 4.トップ画面へリダイレクト
+  redirect_to '/top'
+end
 
   def index
   end
@@ -11,3 +22,7 @@ class ListsController < ApplicationController
   def edit
   end
 end
+  private
+  def list_params
+  params.require(:list).permit(:title,:body)
+  end
